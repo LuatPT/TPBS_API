52a4a8dbf68acf417f07c2e14a3e457c11d71693
LuatPT <luatphamdhv@gmail.com>
2020-07-20 22:01:05 +0700

    20200720 Update construct

--- TPBS_API_MAIN/src/main/java/api/service/TrainerService.java
+++ TPBS_API_MAIN/src/main/java/api/service/TrainerService.java
@@ -3,16 +3,18 @@ package api.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import api.dao.TrainerDao;
 import api.entity.Trainer;
 
 @Service
-public class TrainerService {
-	
+@Repository
+public class TrainerService{
 	@Autowired
-	TrainerDao trainerDao;
+	TrainerDao trainerDao ;
+	
 	public List<Trainer> getAllTrainer() {
 		return trainerDao.getAllTrainerDao();
 	}
